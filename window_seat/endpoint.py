1e79eaa5c2a27f46f98c09a33efa3805b2038483
dchecks <dan.checketts@protonmail.ch>
2022-08-02 11:42:20 -0500

    Upload endpoint

---
+++ window_seat/endpoint.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import File, UploadFile, FastAPI
+
+app = FastAPI()
+
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        await file.close()
+
+    return {"message": f"Successfuly uploaded {file.filename}"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
